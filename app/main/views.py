5483b29b2730126b2c33b7595be56b13b179ec5e
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-17 10:47:33 +0300

    implemented search recipe

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+from re import search
 from flask import render_template,request,redirect,url_for,abort
 from . import root
 from ..models import User,Recipe,Recipe_comment
@@ -26,6 +27,35 @@ def index():
   data= requests.get(url).json()['results']
   print(data)
   
-  return render_template('index.html', recipes =data)
+  search_recipe = request.args.get('recipe_query')
+  if search_recipe:
+        return redirect(url_for('.search', recipe_name=search_recipe ))
+  else:
+      
+    return render_template('index.html', recipes =data)
+
+@root.route('/search/<recipe_name>')
+def search(recipe_name):
+    ''' View function to display the search results '''
+    
+    search_url = 'https://api.spoonacular.com/recipes/complexSearch?query=berries&number=50&apiKey=d7de66eae26449be8fcc6320812e027d'
+    s_data = requests.get(search_url).json()['results']
+    print(s_data)
+    
+    
+    # recipe_name_list = recipe_name.split()
+    # recipe_name_format = "+".join(recipe_name_list)
+    # searched_recipes = s_data
+    title = f'search results for {recipe_name}'
+    return render_template('search.html',recipes = s_data, title=title)
 
 
+
+# @root.route('/recipe/<int:id>')
+# def recipe(id):
+
+#     recipe = get_movie(id)
+#     title = f'{recipe.title}'
+    
+
+#     return render_template('movie.html',title = title,movie = movie, reviews=reviews)
