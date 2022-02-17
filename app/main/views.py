6037b0d2f1ba781623f523b5069a10565ab72c04
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-17 03:57:05 +0300

    new fetch method from API

--- app/main/views.py
+++ app/main/views.py
@@ -6,13 +6,26 @@ from flask_login import login_required,current_user
 from .. import db
 from ..requests import get_recipes
 
+import requests
+import json
+
+
+
 
 
 #views
 @root.route('/')
 def index():
   """view root page function that returns index page and its data"""
+  # recipes= requests.get('https://api.spoonacular.com/food/search?&apiKey=d7de66eae26449be8fcc6320812e027d')
+  # recipe_list = json.loads(recipes.content)
   
-  all_recipes = get_recipes()
-  print(all_recipes)
-  return render_template('index.html', recipes = all_recipes)
+  # all_recipes = get_recipes()
+  # print(all_recipes)
+  url= 'https://api.spoonacular.com/recipes/complexSearch?&apiKey=d7de66eae26449be8fcc6320812e027d&number=8'
+  data= requests.get(url).json()['results']
+  print(data)
+  
+  return render_template('index.html', recipes =data)
+
+
